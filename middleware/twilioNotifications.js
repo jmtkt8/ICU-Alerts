194fbccdfa2d992551d04f54257cb66350a6d4ea
jmtkt8 <jmtkt8@mail.missouri.edu>
2016-09-14 14:30:41 -0500

    Twilio Research
    getting familiar with Twilio API

---
+++ middleware/twilioNotifications.js
@@ -0,0 +1,16 @@
+var twilioClient = require('../twilioClient');
+var fs = require('fs');
+var admins = require('../config/administrators.json');
+
+function formatMessage(errorToReport) {
+  return '[This is a test] ALERT! It appears the server is' +
+    'having issues. Exception: ' + errorToReport;
+};
+
+exports.notifyOnError = function(appError, request, response, next) {
+  admins.forEach(function(admin) {
+    var messageToSend = formatMessage(appError.message);
+    twilioClient.sendSms(admin.phoneNumber, messageToSend);
+  });
+  next(appError);
+};
